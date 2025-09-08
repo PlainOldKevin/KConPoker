76e0640a1c5dbba5d6839eb2a061c197e720790e
unknown <kevinconwaylax@gmail.com>
2025-09-07 23:55:31 -0400

    deckpanel looking good, changed some fonts and colors

--- client/src/components/DeckPanel/DeckPanel.tsx
+++ client/src/components/DeckPanel/DeckPanel.tsx
@@ -1,5 +1,112 @@
 import styles from "./DeckPanel.module.css";
 
 export default function DeckPanel() {
-  return <div className={styles.deck}>Deck Panel Here</div>;
+  return (
+    <div className={styles.deck}>
+      <button id={styles.card} className={styles.hearts}>
+        A♥
+      </button>
+      <button id={styles.card} className={styles.diamonds}>
+        A♦
+      </button>
+      <button id={styles.card}>A♣</button>
+      <button id={styles.card}>A♠</button>
+      <button id={styles.card} className={styles.hearts}>
+        K♥
+      </button>
+      <button id={styles.card} className={styles.diamonds}>
+        K♦
+      </button>
+      <button id={styles.card}>K♣</button>
+      <button id={styles.card}>K♠</button>
+      <button id={styles.card} className={styles.hearts}>
+        Q♥
+      </button>
+      <button id={styles.card} className={styles.diamonds}>
+        Q♦
+      </button>
+      <button id={styles.card}>Q♣</button>
+      <button id={styles.card}>Q♠</button>
+      <button id={styles.card} className={styles.hearts}>
+        J♥
+      </button>
+      <button id={styles.card} className={styles.diamonds}>
+        J♦
+      </button>
+      <button id={styles.card}>J♣</button>
+      <button id={styles.card}>J♠</button>
+      <button id={styles.card} className={styles.hearts}>
+        10♥
+      </button>
+      <button id={styles.card} className={styles.diamonds}>
+        10♦
+      </button>
+      <button id={styles.card}>10♣</button>
+      <button id={styles.card}>10♠</button>
+      <button id={styles.card} className={styles.hearts}>
+        9♥
+      </button>
+      <button id={styles.card} className={styles.diamonds}>
+        9♦
+      </button>
+      <button id={styles.card}>9♣</button>
+      <button id={styles.card}>9♠</button>
+      <button id={styles.card} className={styles.hearts}>
+        8♥
+      </button>
+      <button id={styles.card} className={styles.diamonds}>
+        8♦
+      </button>
+      <button id={styles.card}>8♣</button>
+      <button id={styles.card}>8♠</button>
+      <button id={styles.card} className={styles.hearts}>
+        7♥
+      </button>
+      <button id={styles.card} className={styles.diamonds}>
+        7♦
+      </button>
+      <button id={styles.card}>7♣</button>
+      <button id={styles.card}>7♠</button>
+      <button id={styles.card} className={styles.hearts}>
+        6♥
+      </button>
+      <button id={styles.card} className={styles.diamonds}>
+        6♦
+      </button>
+      <button id={styles.card}>6♣</button>
+      <button id={styles.card}>6♠</button>
+      <button id={styles.card} className={styles.hearts}>
+        5♥
+      </button>
+      <button id={styles.card} className={styles.diamonds}>
+        5♦
+      </button>
+      <button id={styles.card}>5♣</button>
+      <button id={styles.card}>5♠</button>
+      <button id={styles.card} className={styles.hearts}>
+        4♥
+      </button>
+      <button id={styles.card} className={styles.diamonds}>
+        4♦
+      </button>
+      <button id={styles.card}>4♣</button>
+      <button id={styles.card}>4♠</button>
+      <button id={styles.card} className={styles.hearts}>
+        3♥
+      </button>
+      <button id={styles.card} className={styles.diamonds}>
+        3♦
+      </button>
+      <button id={styles.card}>3♣</button>
+      <button id={styles.card}>3♠</button>
+      <button id={styles.card} className={styles.hearts}>
+        2♥
+      </button>
+      <button id={styles.card} className={styles.diamonds}>
+        2♦
+      </button>
+      <button id={styles.card}>2♣</button>
+      <button id={styles.card}>2♠</button>
+    </div>
+  );
 }
