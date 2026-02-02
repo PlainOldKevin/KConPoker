5af55dd5443bd8bd76a773c3af8f28df023f80b4
unknown <kevinconwaylax@gmail.com>
2026-02-01 22:44:10 -0500

    Created card folder, file, stylesheet. added type declaration for card

---
+++ client/src/components/Card/Card.tsx
@@ -0,0 +1,41 @@
+import styles from "./Card.module.css";
+import type { CardId, Rank, Suit } from "../../types/cards.ts";
+import { label } from "../../types/cards.ts";
+
+type CardProps = {
+  // Exactly how the card we are sending should look
+  id: CardId;
+  rank: Rank;
+  suit: Suit;
+  selected: boolean;
+  disabled?: boolean;
+  onClick: (id: CardId) => void;
+};
+
+// Card component
+export default function Card({
+  id,
+  rank,
+  suit,
+  selected,
+  disabled = false,
+  onClick,
+}: CardProps) {
+  const isRedSuit = suit === "h" || suit === "d";
+
+  return (
+    <button
+      type="button"
+      className={`${styles.card} ${isRedSuit ? styles.redSuit : ""} ${
+        selected ? styles.selected : ""
+      } ${disabled ? styles.disabled : ""}`}
+      onClick={() => onClick(id)}
+      disabled={disabled}
+      data-rank={rank}
+      data-suit={suit}
+      aria-pressed={selected}
+    >
+      {label(id)}
+    </button>
+  );
+}
